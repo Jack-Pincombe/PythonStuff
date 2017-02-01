6d644c668dbc3519b375b78f3aff3485bcb86a1e
Jack Pincombe <jackpincombe1@gmail.com>
2017-02-01 16:50:16 +0000

    fibonacci tidied up

--- fibonacci.py
+++ fibonacci.py
@@ -3,10 +3,11 @@
 def fibonacci(n):
     numToPrint = 0
     nextNum = 1
+    print(numToPrint)
     for i in range(n):
         numToPrint += nextNum
         nextNum = numToPrint - nextNum
 
         print(numToPrint)
 
-fibonacci(1000)
+fibonacci(10)
