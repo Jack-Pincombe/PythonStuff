0d96c0dc57fb1139f87552b01d03c939d10f1a1f
Jack Pincombe <jackpincombe1@gmail.com>
2017-02-01 16:44:32 +0000

    Hi szymon

--- fibonacci.py
+++ fibonacci.py
@@ -4,11 +4,8 @@ def fibonacci(n):
     numToPrint = 0
     nextNum = 1
     for i in range(n):
-
-
         numToPrint += nextNum
         nextNum = numToPrint - nextNum
-
 
         print(numToPrint)
 
