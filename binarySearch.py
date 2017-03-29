a26e5f272b307c661f75df8ac28c776459657d42
Jack Pincombe <jackpincombe1@gmail.com>
2017-03-29 22:36:26 +0100

    Just a simple binary search

---
+++ binarySearch.py
@@ -0,0 +1,26 @@
+nums = [12,43,253,2,323432,53,21,24,4,5,2,3,4,999]
+
+
+
+
+def binarySearch(aList,target):
+    #binary search algorithim
+    aList.sort()
+
+    if len(aList) == 0:
+        return False
+    else:
+        m = len(aList) // 2  # gaining the midpoint
+        if aList[m] == target:
+            return True
+        else:
+            if target > aList[m]:
+                return binarySearch(aList[m+1:],target)
+            else:
+                return binarySearch(aList[:m],target)
+
+
+
+
+
+print binarySearch(nums,5)
