e69a92cad378b725bab897e81f4d4308efc279d9
Jack Pincombe <jackpincombe1@gmail.com>
2017-06-22 14:59:46 +0100

    1

--- apiStuff/kittenapi.py
+++ apiStuff/kittenapi.py
@@ -2,4 +2,10 @@ import requests
 
 kittens = requests.get("http://placekitten.com/")
 
-print(kittens.text[559:1000])
+#print(kittens.text[559:1000])
+
+body = {'Name':'Eric', 'Age':'26'}
+
+response = requests.post("http://placekitten.com")
+
+print(response.status_code)
