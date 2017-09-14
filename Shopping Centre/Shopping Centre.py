530cb54c6ad6e7137cbb380bb7385c21614493da
Jack Pincombe <jackpincombe1@gmail.com>
2017-09-14 15:35:16 +0100

    add function added basket added

--- Shopping Centre/Shopping Centre.py
+++ Shopping Centre/Shopping Centre.py
@@ -1,111 +1,122 @@
-'''
-Creating a simple shopping centre simulation
-'''
 import time
-
-basket = {}
-
-areas = ["Beer", "Dairy", "Veg"]
-
-Beer = {
-    "Erdinger" : 4,
-    "Harp": 2.50,
-    "Heinekin": 3
-}
-
-Dairy = {
-    "Milk": 1,
-    "Butter" : 0.50,
-    "Creame": 1
-}
-
-Veg = {
-    "Carrots": 0.25,
-    "Potato": 1,
-    "Turnup": 0.50
-}
-Basket = { }
-
-'''
-A method to print each of the menus
-'''
-def printMenu(x):
-    num = 1
-    for i in x:
-        print(num,"->",i,'£',x[i])
-        num += 1
-    print(len(x) + 1 , '-> Go Back')
+import locale
+result = locale.setlocale(locale.LC_ALL, 'en_GB.UTF-8')
 
 
+beer = [
+    ["Erdinger", 3.00],
+    ["Harp", 3.00],
+    ["Tennents", 2.50]
+]
 
-# function to allow the user to add items to basket
-def options(x):
-    chosen = ''
+dairy = [
+    ["Milk", 1.00],
+    ["Cheese", 2.00],
+    ["Cream", 3.00]
+]
 
-    while ( chosen != "quit" or chosen != 'back'):
-        chosen = input('')
-        if chosen == "1":
+veg = [
+    ["Carrots", 0.10],
+    ["Pumpkin", 2.00 ]
+]
+basket = [
 
-            print(x[int(chosen) - 1])
-            print(Basket)
+]
 
-        elif chosen == '4':
-            mainMenu()
+def intro():
+    print("Hello and welcome to the store")
+
+def whichAisle():
+    print("-----------------------------------------------------------\n"
+          "-----------------------------------------------------------\n"
+          "-----------------------------------------------------------\n")
+    print("1 -> Beer")
+    print("2 -> Dairy")
+    print("3 -> Veg")
+    print("4 -> Checkout")
+    print("5 -> Leave")
+    aisle = input("Please select an aisle ")
+
+    if aisle == '4':
+        returnBasket()
+
+    elif aisle == '5':
+        leave()
+    else:
+        printMenu(aisle)
+
+def leave():
+    print("Good Bye")
+    quit()
+
+def add(aisle):
+
+    if aisle == '4':
+        return whichAisle()
 
 
+    elif aisle == '1':
+        chosen = input('Add -> ')
+        if int(chosen) == len(beer) + 1:
+            whichAisle()
+        basket.append(beer[int(chosen) - 1])
+        return whichAisle()
 
+    elif aisle == '2':
+        chosen = input('Add -> ')
+        if int(chosen) == len(aisle) + 1:
+            whichAisle()
+        basket.append(dairy[int(chosen) - 1])
+        return whichAisle()
 
-def greeting():
-    print("Welcome to ASDA")
-    time.sleep(1)
-
-
-
-
-def mainMenu():
-    print("Which area would you like to view")
-
-    for i in range(len(areas)):
-        print(i + 1, '->' , areas[i])
-        time.sleep(0.1)
-
-    print(len(areas) + 1, "-> Basket")
-
-    print("Enter the corresponding number")
-    option = input("......")
-
-    if int(option) > 3 or int(option) < 1:
-        print("Please enter a valid number")
-        mainMenu()
-
-    elif int(option) == 1:
-
-        printMenu(Beer)
-        options(Beer)
-
-    elif int(option) == 2:
-
-        printMenu(Dairy)
-        options(Dairy)
-    elif int(option) == 3:
-
-        printMenu(Veg)
-        options(Veg)
+    elif aisle == '3':
+        chosen = input('Add -> ')
+        if int(chosen) == len(veg) + 1:
+            whichAisle()
+        basket.append(veg[int(chosen) - 1])
+        return whichAisle()
 
 
 
+    printMenu(aisle)
+
+def returnBasket():
+    for i in basket:
+        print(i)
+
+    total = 0
+    for i in range(len(basket)):
+        total += basket[i][1]
+
+    print("Your total is " ,locale.currency(total))
 
 
-def checkout(bask):
-    pass
 
+def printMenu(aisle):
+
+
+    if aisle == "1":
+        for i in range(len(beer)):
+            print(i + 1 , ' -> ',beer[i])
+        print(len(beer) + 1 , ' -> Go back')
+        add('1')
+
+    elif aisle == '2':
+        for i in range(len(dairy)):
+            print(i + 1 , ' -> ',dairy[i])
+        print(len(dairy) + 1, ' -> Go back')
+        add('2')
+
+    elif aisle == '3':
+        for i in range(len(veg)):
+            print(i + 1 , ' -> ',veg[i])
+        print(len(veg) + 1, ' -> Go back')
+        add("3")
 
 
 
 def __main__():
-    greeting()
-
-    mainMenu()
+    intro()
+    whichAisle()
 
 __main__()
-
