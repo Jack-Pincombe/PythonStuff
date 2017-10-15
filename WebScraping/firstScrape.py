5ad0d5491fbd66f221ebd29a0e62c6806a7b3800
Jack Pincombe <jackpincombe1@gmail.com>
2017-10-15 21:15:29 +0100

    implementing a throw exception into the script

--- WebScraping/firstScrape.py
+++ WebScraping/firstScrape.py
@@ -1,8 +1,21 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
 from bs4 import BeautifulSoup
 
-html = urlopen("http://www.jackpincombe.online")
-bsObj = BeautifulSoup(html.read())
+'''
+Using a try statement to ensure that if the script runs into any issues
+the script will not crash but it will continue and will only throw an exception
+'''
 
-print(bsObj.h1)
+try:
+    html = urlopen("http://www.jackpincombe.online")
+    bsObj = BeautifulSoup(html.read())
+    print(bsObj.h1)
+except HTTPError as e:
+    print(e)
+except URLError as e:
+    print(e)
 
+else:
+    print("It worked")
