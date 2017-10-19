be6a586a9b9d24b06eacca8b22a4e5c743aa9d8c
Jack Pincombe <jackpincombe1@gmail.com>
2017-10-19 13:59:18 +0100

    update

--- ML/firstML.py
+++ ML/firstML.py
@@ -7,7 +7,7 @@ looking at the "Hello World" Machine learning world
 #loading the necessary libraries that we will use
 import pandas
 import pandas
-from pandas.tools.plotting import scatter_matrix
+from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 from sklearn import model_selection
 from sklearn.metrics import classification_report
@@ -25,11 +25,44 @@ url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 names = ['sepal-length', 'sepal-width','petal-length','petal-width','class']
 dataset = pandas.read_csv(url,names=names)
 
-#print(dataset.shape)
-#print(dataset.head(20))
+print(dataset.shape)
+print(dataset.head(20))
 #print(dataset.describe())
 #print(dataset.groupby("class").size())
 
-#dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+#Displaying the dataset in various formats
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
 dataset.hist()
-plt.show()
+scatter_matrix(dataset)
+
+#plt.show()
+
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+seed = 7
+scoring = "accuracy"
+
+#spot checking algorithims
+models = []
+models.append(('LR',LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART',DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+#evaluating each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = model_selection.KFold(n_splits=10, random_state=seed)
+    cv_results = model_selection.cross_val_score(model,X_train,Y_train,cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
