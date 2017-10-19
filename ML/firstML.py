1af7e43f05cb4bffbd713a41df7d23f0da2c0894
Jack Pincombe <jackpincombe1@gmail.com>
2017-10-19 12:01:57 +0100

    looking at some of the functions that can be used to present some of the data

--- ML/firstML.py
+++ ML/firstML.py
@@ -25,5 +25,11 @@ url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 names = ['sepal-length', 'sepal-width','petal-length','petal-width','class']
 dataset = pandas.read_csv(url,names=names)
 
-# print(dataset.shape)
+#print(dataset.shape)
+#print(dataset.head(20))
+#print(dataset.describe())
+#print(dataset.groupby("class").size())
 
+#dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+dataset.hist()
+plt.show()
